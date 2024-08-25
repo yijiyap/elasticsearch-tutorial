394010d3aede0af9e3a8599d457a051e6d16944a
yijiyap <yijiyap@gmail.com>
2024-08-25 18:29:33 +0800

    functions to insert documents

--- search.py
+++ search.py
@@ -19,3 +19,13 @@ class Search:
     def create_index(self):
         self.es.indices.delete(index='my_documents', ignore_unavailable=True)
         self.es.indices.create(index='my_documents')
+    
+    def insert_document(self, document):
+        return self.es.index(index='my_documents', body=document)
+
+    def insert_documents(self, documents):
+        operations = []
+        for document in documents:
+            operations.append({'index': {'_index': 'my_documents'}})
+            operations.append(document)
+        return self.es.bulk(operations=operations)
