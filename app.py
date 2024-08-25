55b1c6d706973b1a8fa8e5796b2ded5cf964c317
yijiyap <yijiyap@gmail.com>
2024-08-25 20:08:05 +0800

    faceted search daamn

--- app.py
+++ app.py
@@ -12,18 +12,64 @@ def index():
 @app.post('/')
 def handle_search():
     query = request.form.get('query', '')
+    filters, parsed_query = extract_filters(query)
     from_ = request.form.get('from_', type=int, default=0)
+
+    if parsed_query:
+        search_query = {
+            'must': {
+                'multi_match': {
+                    'query': parsed_query,
+                    'fields': ['name', 'summary', 'content'],
+                }
+            }
+        }
+    else:
+        search_query = {
+            'must': {
+                'match_all': {}
+            }
+        }
+
     results = es.search(
         query={
-            'multi_match': {
-                'query': query,
-                'fields': ['name', 'summary', 'content'],
+            'bool': {
+                **search_query,
+                **filters
             }
-        }, size=5, from_=from_
+        },
+        aggs={
+            'category-agg': {
+                'terms': {
+                    'field': 'category.keyword',
+                }
+            },
+            'year-agg': {
+                'date_histogram': {
+                    'field': 'updated_at',
+                    'calendar_interval': 'year',
+                    'format': 'yyyy',
+                },
+            },
+        },
+        size=5,
+        from_=from_
     )
+    aggs = {
+        'Category': {
+            bucket['key']: bucket['doc_count']
+            for bucket in results['aggregations']['category-agg']['buckets']
+        },
+        'Year': {
+            bucket['key_as_string']: bucket['doc_count']
+            for bucket in results['aggregations']['year-agg']['buckets']
+            if bucket['doc_count'] > 0
+        },
+    }
     return render_template('index.html', results=results['hits']['hits'],
                            query=query, from_=from_,
-                           total=results['hits']['total']['value'])
+                           total=results['hits']['total']['value'], aggs=aggs)
+
 
 @app.get('/document/<id>')
 def get_document(id):
@@ -38,3 +84,32 @@ def reindex():
     response = es.reindex()
     print(f'Index with {len(response["items"])} documents created in {response["took"]} milliseconds.')
 
+def extract_filters(query):
+    filters = []
+
+    filter_regex = r'category:([^\s]+)\s*'
+    m = re.search(filter_regex, query)
+    if m:
+        filters.append({
+            'term': {
+                'category.keyword': {
+                    'value': m.group(1)
+                }
+            },
+        })
+        query = re.sub(filter_regex, '', query).strip()
+
+    filter_regex = r'year:([^\s]+)\s*'
+    m = re.search(filter_regex, query)
+    if m:
+        filters.append({
+            'range': {
+                'updated_at': {
+                    'gte': f'{m.group(1)}||/y',
+                    'lte': f'{m.group(1)}||/y',
+                }
+            },
+        })
+        query = re.sub(filter_regex, '', query).strip()
+
+    return {'filter': filters}, query
